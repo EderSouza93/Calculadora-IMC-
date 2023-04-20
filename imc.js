fb487d474135ca9c3a080b7264b87b48f584a0a5
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2023-04-20 12:38:37 -0300

    Bug não corrigido retornando ao querySelector

--- imc.js
+++ imc.js
@@ -1,14 +1,14 @@
 
-const calcular = document.getElementById('calcular');
+const calcular = document.querySelector("#calcular");
 
 
 function imc () {
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+    const nome = document.querySelector("#nome").value;
+    const altura = document.querySelector("#altura").value;
+    const peso = document.querySelector("#peso").value;
+    const resultado = document.querySelector("#resultado");
 
-    if(nome !== '' && altura !== '' && peso !== '') {
+    if(nome !== "" && altura !== "" && peso !== "") {
         alert ('valor preenchido!');
     }else {
         resultado.textContent = 'Preencha todos os campos!!!';
