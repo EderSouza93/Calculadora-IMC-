2b764b32b44c15c5235c72abb737bde184445fb9
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2023-04-20 12:27:21 -0300

    Validação corrigida e finalizada

--- imc.js
+++ imc.js
@@ -3,12 +3,12 @@ const calcular = document.querySelector("#calcular");
 
 
 function imc () {
-    const nome = document.querySelector("#nome");
-    const altura = document.querySelector("#altura");
-    const peso = document.querySelector("#peso");
-    const resultado = document.querySelector("#resultado");
+    const nome = document.querySelector("#nome").value;
+    const altura = document.querySelector("#altura").value;
+    const peso = document.querySelector("#peso").value;
+    const resultado = document.querySelector("#resultado").value;
 
-    if(nome.value !== '' && altura !== '' && peso !== '') {
+    if(nome !== '' && altura !== '' && peso !== '') {
         alert ('valor preenchido!');
     }else {
         resultado.textContent = 'Preencha todos os campos!!!';
