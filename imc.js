89fe551ebbe86f9e5f6ac7653bc5c6b62a860997
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2023-04-20 12:50:38 -0300

    Definindo o value na função para verificar se corrige o bug

--- imc.js
+++ imc.js
@@ -3,18 +3,17 @@ const calcular = document.querySelector("#calcular");
 
 
 function imc () {
-    const nome = document.querySelector("#nome").value;
-    const altura = document.querySelector("#altura").value;
-    const peso = document.querySelector("#peso").value;
+    const nome = document.querySelector("#nome");
+    const altura = document.querySelector("#altura");
+    const peso = document.querySelector("#peso");
     const resultado = document.querySelector("#resultado");
 
-    if(nome !== "" && altura !== "" && peso !== "") {
+    if(nome.value !== "" && altura.value !== "" && peso.value !== "") {
         alert ('valor preenchido!');
     }else {
         alert('Preencha todos os campos!!');
     }
    
 }
-imc()
 
 calculate.addEventListener('click', imc);
