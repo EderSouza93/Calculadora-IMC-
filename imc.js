443e98777b1f2bc930e9be383d76c2054740ee9b
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2023-04-20 11:47:53 -0300

    Teste do código JavaScript

--- imc.js
+++ imc.js
@@ -1,3 +1,7 @@
 const name = document.querySelector("#nome");
 const height = document.querySelector("#altura");
-const weight = document.querySelector("#peso")
+const weight = document.querySelector("#peso");
+const calculate = document.querySelector("#calcular");
+const result = document.querySelector("#resultado");
+
+alert(name);
