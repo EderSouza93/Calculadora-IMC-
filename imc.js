38e6af9f402b63b4b63fcb6dea1d66ff4ed0bd94
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2023-04-20 12:30:47 -0300

    Validação corrigida 2

--- imc.js
+++ imc.js
@@ -6,7 +6,7 @@ function imc () {
     const nome = document.querySelector("#nome").value;
     const altura = document.querySelector("#altura").value;
     const peso = document.querySelector("#peso").value;
-    const resultado = document.querySelector("#resultado").value;
+    const resultado = document.querySelector("#resultado");
 
     if(nome !== '' && altura !== '' && peso !== '') {
         alert ('valor preenchido!');
